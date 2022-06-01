2eb179d9ea8daf4f7252c4e8727e530b6af5e06b
Gus1616 <90276026+Gus1616@users.noreply.github.com>
2022-06-01 20:29:31 +0100

    updates

--- todoList1/settings.py
+++ todoList1/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+LOGIN_URL = 'login'
+
+
 STATIC_URL = '/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
